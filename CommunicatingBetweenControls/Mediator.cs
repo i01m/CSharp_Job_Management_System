d7c402e3cf6449d8b59f7c907c22dd7220f8a856
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 13:44:04 -0500

    first commit

---
+++ CommunicatingBetweenControls/Mediator.cs
@@ -0,0 +1,40 @@
+﻿using CommunicatingBetweenControls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicatingBetweenControls
+{
+    public sealed class Mediator
+    {
+        //static members
+        private static readonly Mediator _Instance = new Mediator();
+        private Mediator() { }
+
+        public static Mediator GetInstance()
+        {
+            return _Instance;
+        }
+
+        //Instance funtionality
+        public event EventHandler<JobChangedEventArgs> JobChanged;
+
+        public void OnJobChanged(object sender, Job job)
+        {
+            //--option 1 with using delegate
+            //var JobChangedDelegate = JobChanged as EventHandler<JobChangedEventArgs>;
+            //if (JobChangedDelegate != null)
+            //{
+            //    JobChangedDelegate(sender, new JobChangedEventArgs { Job = job });
+            //}
+
+            //--option 2 - using only event
+            if (JobChanged != null)
+            {
+                JobChanged(sender, new JobChangedEventArgs { Job = job });
+            }
+        }
+    }
+}
